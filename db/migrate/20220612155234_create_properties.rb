9170c521df0be62f3b11c2e6d1af2f83ee9be5ec
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-06-14 07:56:01 -0300

    Parte1

---
+++ db/migrate/20220612155234_create_properties.rb
@@ -0,0 +1,16 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.references :account
+      t.string :name
+      t.string :address
+      t.integer :price
+      t.integer :rooms
+      t.integer :bathrooms
+      t.string :photo
+
+
+      t.timestamps
+    end
+  end
+end
