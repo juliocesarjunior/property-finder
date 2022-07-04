b0d149d313f6c3a33370eedc991c844a06d36211
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-07-04 07:36:34 -0300

    Parte1(upload add)

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,50 +1,69 @@
 class PhotoUploader < CarrierWave::Uploader::Base
-    # Include RMagick or MiniMagick support:
-    # include CarrierWave::RMagick
-    # include CarrierWave::MiniMagick
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
-    include CarrierWave::MiniMagick
-    include CarrierWave::ImageOptimizer
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
 
-    process :optimize
-    
-    # Choose what kind of storage to use for this uploader:
-    storage :file
-    # storage :fog
-
-    # Override the directory where uploaded files will be stored.
-    # This is a sensible default for uploaders that are meant to be mounted:
-    # Local onde será guardado as imagens
-    def store_dir
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
+  end
 
-    ## Tamanhos que o CarrierWave vai salvar as imagens
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
 
-    # Thumb
-    version :thumb do
-     process resize_to_fit: [50, 50]
-    end
+  # Process files as they are uploaded:
+  # process scale: [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
 
-    # Medium
-    version :medium do
-     process resize_to_fit: [150, 150]
-    end
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
-    # Big
-    version :big do
-     process resize_to_fit: [300, 300]
-    end
+  # Add an allowlist of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_allowlist
+  #   %w(jpg jpeg gif png)
+  # end
 
-    # Tipos de extensão aceitas
-    def extension_whitelist
-     %w(jpg jpeg gif png)
-    end
+  ## Tamanhos que o CarrierWave vai salvar as imagens
 
+  # Thumb Pequena
+  version :thumb do
+   process resize_to_fit: [50, 50]
+  end
 
-    # Override the filename of the uploaded files:
-    # Avoid using model.id or version_name here, see uploader/store.rb for details.
-    # def filename
-    #   "something.jpg" if original_filename
-    # end
+  # Medium
+  version :medium do
+   process resize_to_fit: [150, 150]
+  end
+
+  # Big
+  version :big do
+   process resize_to_fit: [300, 300]
+  end
+
+  # Tipos de extensão aceitas
+  def extension_whitelist
+   %w(jpg jpeg gif png)
+  end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
 end
