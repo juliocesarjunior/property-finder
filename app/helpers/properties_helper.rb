949f853be145c300c25c1ed946759edfa0e39fcb
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-07-04 12:01:00 -0300

    Parte1(Pagina Inicial)

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,7 @@
 module PropertiesHelper
+
+	def property_thumbnail property
+		img = property.photo.present? ? property.photo.url : "placeholder.png"
+		image_tag img, class: "property_thumb"
+	end
 end
