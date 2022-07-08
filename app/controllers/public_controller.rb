b57c9908cfdac4ae8d6ee8323ac11bf44f62d135
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-07-08 12:07:34 -0300

    Parte3(Inicio)

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,8 @@
 class PublicController < ApplicationController
   def main
+    if account_signed_in?
+      redirect_to dashboard_path, flash: { sucess: "Successfully signed in. Welcome to Property FInder!"} and return
+    end
     @properties = Property.latest
   end
 end
