8e5d46c78ade1722f87d6c51ed3f79344a326664
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-07-08 17:22:10 -0300

    Parte3(Alerts)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
 		image_tag thumb, class: "profile-pic img-circle"
 	end
 
+	def flash_class(level)
+		bootstrap_alert_class = {
+			"success" => "alert-success",
+			"error" => "alert-danger",
+			"notice" => "alert-info",
+			"alert" => "alert-danger",
+			"warn" => "alert-warning"
+		}
+		bootstrap_alert_class[level]
+	end
+
 end
